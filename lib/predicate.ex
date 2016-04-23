986b0fd18d65b9533d323398eb08f20d5ecf0fd0
andri setiawan <andrisetiawan@me.com>
2016-04-23 19:24:52 +0700

    simple predicate

--- lib/predicate.ex
+++ lib/predicate.ex
@@ -1,2 +1,18 @@
 defmodule Predicate do
+  # def is(x, 'empty'), do: is_contain(x, y)
+  # def is(x, 'not_empty'), do: is_contain(x, y)
+
+  def is(x, 'equal', y), do: x == y
+  def is(x, 'not_equal', y), do: x != y
+  def is(x, 'less_than', y), do: x < y
+  def is(x, 'greater_than', y), do: x > y
+  def is(x, 'less_than_or_equal_to', y), do: x <= y
+  def is(x, 'greater_than_or_equal_to', y), do: x >= y
+  def is(x, 'in', y), do: is_contain(y, x)
+  def is(x, 'contain', y), do: is_contain(x, y)
+
+
+  defp is_contain(enum, element) do
+    Enum.any?(enum, fn(el) -> el == element end)
+  end
 end
--- test/predicate_test.exs
+++ test/predicate_test.exs
@@ -2,7 +2,50 @@ defmodule PredicateTest do
   use ExUnit.Case
   doctest Predicate
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "equal" do
+    assert Predicate.is('a', 'equal', 'a') == true
+    assert Predicate.is(2, 'equal', 2) == true
+    assert Predicate.is({'a', 2}, 'equal', {'a', 2}) == true
+  end
+
+  test "not_equal" do
+    assert Predicate.is('a', 'not_equal', 'b') == true
+    assert Predicate.is(2, 'not_equal', 3) == true
+    assert Predicate.is({'a', 2}, 'not_equal', {'a', 3}) == true
+    assert Predicate.is({'a', 2}, 'not_equal', {'a'}) == true
+  end
+
+  test "less_than" do
+    assert Predicate.is(1, 'less_than', 2) == true
+    assert Predicate.is(2, 'less_than', 2) == false
+    assert Predicate.is(2, 'less_than', 1) == false
+  end
+
+  test "greater_than" do
+    assert Predicate.is(1, 'greater_than', 2) == false
+    assert Predicate.is(2, 'greater_than', 2) == false
+    assert Predicate.is(2, 'greater_than', 1) == true
+  end
+
+  test "less_than_or_equal_to" do
+    assert Predicate.is(1, 'less_than_or_equal_to', 2) == true
+    assert Predicate.is(2, 'less_than_or_equal_to', 2) == true
+    assert Predicate.is(2, 'less_than_or_equal_to', 1) == false
+  end
+
+  test "greater_than_or_equal_to" do
+    assert Predicate.is(1, 'greater_than_or_equal_to', 2) == false
+    assert Predicate.is(2, 'greater_than_or_equal_to', 2) == true
+    assert Predicate.is(2, 'greater_than_or_equal_to', 1) == true
+  end
+
+  test "in" do
+    assert Predicate.is('a', 'in', ['a', 'b']) == true
+    assert Predicate.is('c', 'in', ['a', 'b']) == false
+  end
+
+  test "contain" do
+    assert Predicate.is(['a', 'b'], 'contain', 'a') == true
+    assert Predicate.is(['a', 'b'], 'contain', 'c') == false
   end
 end
